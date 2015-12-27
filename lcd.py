3f3edfe9bceb531fed9dd2dcaafca7782afaed67
Macbook <hsmptg@gmail.com>
2015-12-27 13:07:47 +0000

    Remove linhas comentadas

--- lcd.py
+++ lcd.py
@@ -7,11 +7,6 @@ lcd.clear()
 lcd.display(True)
 lcd.invert(False)
 
-#lcd.printat(1, 3, "EDM 2015")
-#lcd.printat(2, 1, "Project: LCD")
-#lcd.printat(3, 3, time.strftime("%H:%M:%S"))
-#lcd.printat(4, 2, time.strftime("%d/%m/%Y"))
-
 lcd.printat(0, 0, 'IP: 192.168.102.123')
 
 lcd.print2at(2, 2, 'X: 123')
